51d788a8cf2f2d42de62fd6d920fbed4268fdcab
Ezequiel <crespo.ep@gmail.com>
2020-04-14 15:47:16 -0300

    initial commit

---
+++ src/OdiseoSyliusEasyPostPlugin.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Odiseo\SyliusEasyPostPlugin;
+
+use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class OdiseoSyliusEasyPostPlugin extends Bundle
+{
+    use SyliusPluginTrait;
+}
